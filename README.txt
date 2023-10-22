7ff856ac852ac865652607f91b51cbfa4f976398
Uni-Lucas <844665@unizar.es>
2023-10-22 13:35:13 +0200

    basic routes

--- README.txt
+++ README.txt
@@ -1,2 +1,9 @@
 --------- INSTALL --------
 bundle install
+
+
+--------- RUN SERVER -----
+Declarar variables de entorno de la BD
+
+source export_vars.sh
+bin/rails server
--- appetizr/config/routes.rb
+++ appetizr/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root "categories#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/platos", to: "platos#index"
   resources :categories, param: :nombre
+  resources :restaurants
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
