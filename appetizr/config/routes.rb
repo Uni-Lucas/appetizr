04e1b87e15c5699b9d007c2cb4082e4112dbfe10
Uni-Lucas <844665@unizar.es>
2023-10-27 12:21:15 +0200

    asdfdsa

--- appetizr/config/routes.rb
+++ appetizr/config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :categories, param: :nombre
   resources :restaurants
-
+  
+  get '/registrarse', to: 'registrations#new'
+  post '/registrarse', to: 'regsitrations#create'
+  get '/login', to: 'login#new'
+  post '/login', to: 'login#create'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
