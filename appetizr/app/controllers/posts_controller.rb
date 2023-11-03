cb12bf4caa2d478d03cfe7c28cb1f88e1cb11f17
daniihc16 <danihercecruz@gmail.com>
2023-11-03 21:00:54 +0100

    Ya van las putas imagenes en los posts me cago en github

--- appetizr/app/controllers/posts_controller.rb
+++ appetizr/app/controllers/posts_controller.rb
@@ -5,7 +5,10 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(categoria: session[:post_category], autor: session[:username], contenido: params[:post][:content], ruta_img: params[:post][:image], created_at: Time.now, updated_at: Time.now)
+    nombre_imagen = subir_imagen(params.require(:post)[:image])
+    usuario = User.find_by(nombre: session[:username])
+    # Cambiar en un futuro para que el usuario sea el post añada el campo de comentado por restaurante
+    @post = Post.new(categoria: session[:post_category], autor: session[:username], contenido: params[:post][:content], ruta_img: nombre_imagen, created_at: Time.now, updated_at: Time.now)
 
     session[:post_category] = nil
     if @post.save
@@ -22,4 +25,18 @@ class PostsController < ApplicationController
     params.require(:post).permit(:categoria, :autor, :contenido, :image)
   end
 
+  def subir_imagen(imagen)
+    begin
+      nombre_imagen = SecureRandom.hex(10) + File.extname(imagen.original_filename)
+      ruta = File.join("app", "assets", "images", nombre_imagen)
+      File.open(ruta, "wb") do |f| 
+         f.write(imagen.read)
+      end
+      nombre_imagen
+    
+    rescue => e
+      Rails.logger.error "Error al subir la imagen: #{e.message}"
+      nil
+    end
+  end
 end
