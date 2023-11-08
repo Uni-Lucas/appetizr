e1e62239a0da50d1e72d9750c43ee7de8bfd1afd
Str1ien <villagrasaalain@gmail.com>
2023-11-08 20:19:23 +0100

    hehe

--- appetizr/app/controllers/users_controller.rb
+++ appetizr/app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
     end
 
     def create
-      @user = User.new(nombre: params[:user][:nombre], password: params[:user][:password], ruta_img_perfil: "default", esAdmin: false, password_confirmation: params[:user][:password_confirmation])
+      nombre_imagen = subir_imagen(params.require(:user)[:ruta_img_perfil])
+      @user = User.new(nombre: params[:user][:nombre], password: params[:user][:password], ruta_img_perfil: nombre_imagen, esAdmin: false, password_confirmation: params[:user][:password_confirmation])
 
       if @user.save
         session[:username] = @user.nombre
@@ -23,13 +24,15 @@ class UsersController < ApplicationController
     end
 
     def update
+      nombre_imagen = subir_imagen(params.require(:user)[:ruta_img_perfil])
       @user = User.find_by(nombre: session[:username])
+      
       if !@user.update(user_params)
         render :edit, status: unprocessable_entity
       end
     end
     private
     def user_params
-        params.require(:user).permit(:nombre, :password, :password_confirmation, :profile_pic)
+        params.require(:user).permit(:nombre, :password, :password_confirmation, :ruta_img_perfil)
     end
 end
