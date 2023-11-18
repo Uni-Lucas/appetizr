cf9a6c3bb474cca407e3aadf425a500e55048daf
daniihc16 <danihercecruz@gmail.com>
2023-11-18 23:40:28 +0100

    Actualiza la imagen al subir una imagen nueva pero no deja hacer form.submit, el botón no clica

--- appetizr/db/migrate/20231118200254_add_imported_id_to_restaurants.rb
+++
@@ -1,5 +0,0 @@
-class AddImportedIdToRestaurants < ActiveRecord::Migration[7.1]
-  def change
-    add_column :restaurants, :imported_id, :integer, default: nil
-  end
-end
--- appetizr/db/migrate/20231118200256_add_imported_id_to_dishes.rb
+++
@@ -1,5 +0,0 @@
-class AddImportedIdToDishes < ActiveRecord::Migration[7.1]
-  def change
-    add_column :dishes, :imported_id, :integer, default: nil
-  end
-end
