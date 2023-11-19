65e8ce9aa1ccde3017ef84309bce46293411d9f1
Uni-Lucas <844665@unizar.es>
2023-11-19 23:14:42 +0100

    elimina todo de las categorias

--- appetizr/app/models/category.rb
+++ appetizr/app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   self.primary_key = "nombre"
   
-  has_many :restaurants, class_name: 'Restaurant', foreign_key: 'categoria'
-  has_many :posts, class_name: 'Post', foreign_key: 'categoria'
+  has_many :restaurants, class_name: 'Restaurant', foreign_key: 'categoria', dependent: :nullify
+  has_many :posts, class_name: 'Post', foreign_key: 'categoria', dependent: :destroy
   validates :nombre, presence: true
 end
