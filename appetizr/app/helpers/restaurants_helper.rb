7c7f4e1a5c664749ade4ad033aeeae248acd7025
Str1ien <villagrasaalain@gmail.com>
2023-11-24 19:24:04 +0100

    cositas

--- appetizr/app/helpers/restaurants_helper.rb
+++ appetizr/app/helpers/restaurants_helper.rb
@@ -1,7 +1,7 @@
 module RestaurantsHelper
   def get_rest_managers_posts(rest_id)
     results = ActiveRecord::Base.connection.execute("SELECT posts.* FROM posts, users, restaurants_users as ru WHERE ru.user_id=users.nombre AND ru.restaurant_id=#{rest_id} AND posts.autor=users.nombre AND posts.restaurant_id=#{rest_id}")
-    vals = results.values.map{|post| Post.new(id: post[0], categoria: post[1], autor: post[2], contenido: post[3], ruta_img: post[4], created_at: post[5], updated_at: post[6])}
+    vals = results.values.map{|post| Post.new(id: post[0], categoria: post[1], autor: post[2], contenido: post[3], ruta_img: post[4], created_at: post[5], updated_at: post[6], restaurant_id: post[7])}
     return vals
   end
   def get_total_reviews(rest)
